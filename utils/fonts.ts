222ffb60e2dd91513ac04db9f4303a7b482bff5c
nqhung291 <nqhung291@gmail.com>
2023-05-06 01:40:00 +0700

    chore: refactor fonts

---
+++ utils/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Open_Sans } from "next/font/google"
+
+export const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+})
+
+export const openSans = Open_Sans({
+  subsets: ["latin"],
+  variable: "--font-open-sans",
+})
